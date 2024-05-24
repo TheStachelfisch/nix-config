d8ecf3a0a3e92850af43b3297e248c03dd1bba37
TheStachelfisch <contact@thestachelfisch.dev>
2024-05-24 20:43:51 +0200

    Add keepassxc Secret Service Autostart

--- home/stachel/desktop/common/apps/keepassxc.nix
+++ home/stachel/desktop/common/apps/keepassxc.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ keepassxc ];
+  # This doesn't launch with Wayland support for some reason, so only used as backup
+  home.file.".local/share/dbus-1/services/org.freedesktop.secrets.service".text = ''
+  [D-BUS Service]
+  Name=org.freedesktop.secrets
+  Exec=${pkgs.keepassxc}/bin/keepassxc -platform wayland
+  '';
 }
