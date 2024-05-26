46f8dbe07a2ab1eb96f5c686ac51928a90c0ac60
TheStachelfisch <contact@thestachelfisch.dev>
2024-05-26 15:24:15 +0200

    feat(overlays): add keepassxc overlay for newest version

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,5 +4,17 @@
     vimPlugins = prev.vimPlugins // final.callPackage ../pkgs/vim-plugins { };
   };
 
-  modifications = final: prev: { };
+  modifications = final: prev: {
+    keepassxc = prev.keepassxc.overrideAttrs (oldAttrs: rec {
+      version = "2.7.8";
+      src = prev.fetchFromGitHub {
+        owner = "keepassxreboot";
+        repo = "keepassx";
+        rev = version;
+        hash = "sha256-Gb5/CPhn/phVVvz9BFv7rb12n/P3rPNl5r2gA+E5b0o=";
+      };
+
+      buildInputs = oldAttrs.buildInputs ++ [ final.keyutils ];
+    });
+  }; 
 }
