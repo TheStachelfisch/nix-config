4658958a62145f8b4e32e0a4834064c5bb5f8f74
TheStachelfisch <contact@thestachelfisch.dev>
2024-06-07 17:03:29 +0200

    feat(hosts/framework): add default scale for built-in monitor

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -4,9 +4,13 @@
     ../../home/stachel/desktop/hyprland-material # Use Hyprland setup
   ];
 
+  wayland.windowManager.hyprland.settings = {
+    monitor = [ "eDP-1,2560x1600@165,0x0,1.6,vrr,1" ];
+  };
+
   theme = {
     enable = true;
-    wallpaper = ./wallpapers/new2.jpg;
+    wallpaper = ./wallpapers/test.png;
     variant = "dark";
   };
 }
