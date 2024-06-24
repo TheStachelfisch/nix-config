90330ca09e749fa22dc5d88d29bbeda5190a4403
TheStachelfisch <contact@thestachelfisch.dev>
2024-06-24 14:41:12 +0200

    feat(hosts/framework): add hidpi specific changes for apps and xwayland

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -6,6 +6,13 @@
 
   wayland.windowManager.hyprland.settings = {
     monitor = [ "eDP-1,2560x1600@165,0x0,1.6,vrr,1" ];
+    xwayland = {
+      force_zero_scaling = true;
+    };
+    env = [
+      "GDK_SCALE,1.6"
+      "STEAM_FORCE_DESKTOPUI_SCALING,1.6"
+    ];
   };
 
   theme = {
