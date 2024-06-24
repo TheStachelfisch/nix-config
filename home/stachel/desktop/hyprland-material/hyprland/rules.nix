06a97037e273c23f3499f0bda2f1bb8a55f21ba8
TheStachelfisch <contact@thestachelfisch.dev>
2024-06-24 14:57:17 +0200

    feat(hyprland-material/hyprland): fix rules and add new rules for windows and layers

--- home/stachel/desktop/hyprland-material/hyprland/rules.nix
+++ home/stachel/desktop/hyprland-material/hyprland/rules.nix
@@ -3,17 +3,26 @@
     windowrulev2 = [
       # No borders on unfocused windows, since they are set to full alpha anyway
       "noborder, focus:0 floating:1"
+      # Dim around polkit screen
       "dimaround, title:(Authenticate)"
-      "noanim, title:(rofi-launcher)"
+      # KeepassXC
+      "center 1, title:(Unlock Database|Access Request)"
+      "float, title:(Unlock Database|Access Request)"
+      "stayfocused, class:^(pinentry-:)" # fix pinentry losing focus
     ];
 
     layerrule = [
       "blur,gtk-layer-shell"
       "blur,rofi"
       "ignorezero,gtk-layer-shell"
-      "ignorezero,rofi"
-      "dimaround,rofi"
-      "animation slide,rofi"
+
+      "animation slide,eww-control-center"
+      "blur,eww-window"
+      "ignorealpha 0.6,eww-window"
+      "xray 1,eww-window"
+
+      "blur,rofi"
+      "ignorealpha 0.6,rofi"
     ];
   };
 }
