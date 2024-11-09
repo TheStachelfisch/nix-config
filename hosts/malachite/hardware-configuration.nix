ae8c268ccb3dcea7f2cc9a61d06b1bf46eab910a
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-09 22:45:10 +0100

    refactor(hosts/malachite): redeploy host

--- hosts/malachite/hardware-configuration.nix
+++ hosts/malachite/hardware-configuration.nix
@@ -1,24 +1,15 @@
+{ modulesPath, lib, ... }:
 {
-  modulesPath,
-  lib,
-  ...
-}: {
-  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
     device = "nodev";
-    configurationLimit = 1;
   };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/A552-E981";
-    fsType = "vfat";
-  };
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
-  boot.initrd.kernelModules = ["nvme"];
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
+  fileSystems."/boot/efi" = { device = "/dev/disk/by-uuid/A552-E981"; fsType = "vfat"; };
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
