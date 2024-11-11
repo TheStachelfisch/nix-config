2deb5778848be1762553a379afc08af876670817
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-11 13:00:29 +0100

    fix(hosts/common): temporary fix for storagebox not being writeable

--- hosts/common/optional/storagebox.nix
+++ hosts/common/optional/storagebox.nix
@@ -32,6 +32,6 @@
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},uid=1001,gid=100,credentials=${config.sops.secrets.personal-storagebox.path}"];
+    in [ "${automount_opts},uid=1002,gid=100,credentials=${config.sops.secrets.personal-storagebox.path}" ];
   };
 }
