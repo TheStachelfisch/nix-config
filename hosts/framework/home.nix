98a09fbda433f2369c8a5a8ace485fd43837de86
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-14 09:57:58 +0100

    feat(hosts/framework): add patched fontset

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -3,4 +3,11 @@
     ../../home/thestachelfisch
     ../../home/thestachelfisch/features/desktop/plasma
   ];
+
+  home.packages = with pkgs; [
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+  ];
 }
