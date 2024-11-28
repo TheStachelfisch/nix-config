6610d838b471d6994343956507c13d2b4544590e
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-29 00:47:08 +0100

    feat(overlays): add pkgs.inputs flake overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,24 @@
   inputs,
   outputs,
 }: {
+  # From https://github.com/Misterio77/nix-config/blob/eb20842094f8963d9231ed8bf5e682ee83619f92/overlays/default.nix#L13
+  # For every flake input, aliases 'pkgs.inputs.${flake}' to
+  # 'inputs.${flake}.packages.${pkgs.system}' or
+  # 'inputs.${flake}.legacyPackages.${pkgs.system}'
+  flake-inputs = final: _: {
+    inputs =
+      builtins.mapAttrs (
+        _: flake: let
+          legacyPackages = (flake.legacyPackages or {}).${final.system} or {};
+          packages = (flake.packages or {}).${final.system} or {};
+        in
+          if legacyPackages != {}
+          then legacyPackages
+          else packages
+      )
+      inputs;
+  };
+
   # Adds pkgs.stable as an option
   stable = final: _: {
     stable = inputs.nixpkgs-stable.legacyPackages.${final.system};
