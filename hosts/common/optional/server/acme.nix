c0733e55e9c18a95ff5ac1b0184734844622f803
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-29 01:26:56 +0100

    fix(hosts/common): fix acme defaults secrets

--- hosts/common/optional/server/acme.nix
+++ hosts/common/optional/server/acme.nix
@@ -4,7 +4,11 @@
     acceptTerms = true;
     defaults.email = "me@thestachelfisch.dev";
     defaults.dnsProvider = "cloudflare";
-    defaults.environmentFile = config.sops.secrets.cloudflare;
+    defaults.environmentFile = config.sops.secrets.cloudflare.path;
     defaults.dnsResolver = "1.1.1.1:53";
   };
+
+  sops.secrets.cloudflare = {
+    sopsFile = ../../secrets.yaml;
+   };
 }
