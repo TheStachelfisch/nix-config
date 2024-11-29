1d55a74aa5fddc5e7849c16710c19752a3c0531d
TheStachelfisch <contact@thestachelfisch.dev>
2024-11-29 01:14:48 +0100

    feat(hosts/common): add default acme dns provider and api token

--- hosts/common/optional/server/acme.nix
+++ hosts/common/optional/server/acme.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   security.acme = {
     acceptTerms = true;
     defaults.email = "me@thestachelfisch.dev";
+    defaults.dnsProvider = "cloudflare";
+    defaults.environmentFile = config.sops.secrets.cloudflare;
+    defaults.dnsResolver = "1.1.1.1:53";
   };
 }
