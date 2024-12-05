6adbd4cb9bce8dc0ed7a70466964e6ee30754926
TheStachelfisch <contact@thestachelfisch.dev>
2024-12-05 08:44:33 +0100

    feat(hosts/common/plasma): add colord

--- hosts/common/optional/desktop/plasma.nix
+++ hosts/common/optional/desktop/plasma.nix
@@ -11,4 +11,7 @@
   services.desktopManager.plasma6.enable = true;
 
   programs.dconf.enable = true;
+
+  services.colord.enable = true;
+  environment.systemPackages = with pkgs; [ kdePackages.colord-kde ];
 }
