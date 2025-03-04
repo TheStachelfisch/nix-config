52f021e82ece6acb9a129ea8790c4aa820747303
TheStachelfisch <contact@thestachelfisch.dev>
2025-03-04 16:49:56 +0100

    change storagebox uid

--- hosts/common/optional/storagebox.nix
+++ hosts/common/optional/storagebox.nix
@@ -10,11 +10,11 @@
 
   environment.systemPackages = with pkgs; [cifs-utils];
 
-  fileSystems."/mnt/storagebox" = {
+  fileSystems."/mnt/Storagebox" = {
     device = "//u337764-sub1.your-storagebox.de/u337764-sub1";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},uid=1002,gid=${toString config.users.groups.users.gid},credentials=${config.sops.secrets.personal-storagebox.path}"];
+    in ["${automount_opts},uid=1000,gid=${toString config.users.groups.users.gid},credentials=${config.sops.secrets.personal-storagebox.path},comment=x-gvfs-show"];
   };
 }
