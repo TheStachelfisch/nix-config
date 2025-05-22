b40d50083dab28ad0b5f4984daf2b87c6c80b2e1
TheStachelfisch <contact@thestachelfisch.dev>
2025-05-23 00:05:36 +0200

    add mysql database to wsl

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -4,10 +4,10 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 {
-  config,
-  lib,
-  pkgs,
-  ...
+config,
+lib,
+pkgs,
+...
 }: {
   imports = [
     ../common/global
@@ -26,11 +26,20 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    ensureUsers = [
+      {
+        name = "root";
+        ensurePermissions = {
+          "*.*" = "ALl PRIVILEGES";
+        };
+      }
+    ];
+  };
+
+  systemd.services.mysql.wantedBy = lib.mkForce [];
+
   system.stateVersion = "24.05"; # Did you read the comment?
 }
