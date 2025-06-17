6c15c3a25bcd7b9f5fab88b59e40db1b7d94e8c0
TheStachelfisch <contact@thestachelfisch.dev>
2025-06-17 09:17:56 +0200

    override libplacebo version to fix moonlight

--- overlays/default.nix
+++ overlays/default.nix
@@ -48,6 +48,25 @@ in {
       buildInputs = oldAttrs.buildInputs ++ [final.keyutils];
     });
 
+    mpv-unwrapped = prev.mpv-unwrapped.override {
+      libplacebo = final.libplacebo-mpv;
+    };
+
+    libplacebo-mpv =
+      let
+      version = "7.349.0";
+    in
+      prev.libplacebo.overrideAttrs (old: {
+          inherit version;
+          src = prev.fetchFromGitLab {
+          domain = "code.videolan.org";
+          owner = "videolan";
+          repo = "libplacebo";
+          rev = "v${version}";
+          hash = "sha256-mIjQvc7SRjE1Orb2BkHK+K1TcRQvzj2oUOCUT4DzIuA=";
+          };
+          });
+
     klipper = prev.klipper.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [(prev.python3.withPackages (
         p: with p; [
