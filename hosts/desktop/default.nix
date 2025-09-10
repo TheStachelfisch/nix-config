59245fb9c08a8c03ebde9a82ccd210929db7a025
TheStachelfisch <contact@thestachelfisch.dev>
2025-09-10 13:27:34 +0200

    update desktop configuration

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,4 +1,44 @@
-{pkgs, config, ...}: {
+{pkgs, config, ...}: let
+  orcaSlicerDesktopItem = pkgs.makeDesktopItem {
+    name = "orca-slicer-zink";
+    desktopName = "OrcaSlicer";
+    genericName = "3D Printing Software";
+    icon = "OrcaSlicer";
+    exec = "env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1 ${pkgs.orca-slicer}/bin/orca-slicer %U";
+    terminal = false;
+    type = "Application";
+    mimeTypes = [
+      "model/stl"
+      "model/3mf"
+      "application/vnd.ms-3mfdocument"
+      "application/prs.wavefront-obj"
+      "application/x-amf"
+      "x-scheme-handler/orcaslicer"
+    ];
+    categories = [ "Graphics" "3DGraphics" "Engineering" ];
+    keywords = [ "3D" "Printing" "Slicer" "slice" "3D" "printer" "convert" "gcode" "stl" "obj" "amf" "SLA" ];
+    startupNotify = false;
+    startupWMClass = "orca-slicer";
+  };
+
+  mimeappsListContent = ''
+    [Default Applications]
+    model/stl=orca-slicer-dri.desktop;
+    model/3mf=orca-slicer-dri.desktop;
+    application/vnd.ms-3mfdocument=orca-slicer-dri.desktop;
+    application/prs.wavefront-obj=orca-slicer-dri.desktop;
+    application/x-amf=orca-slicer-dri.desktop;
+
+    [Added Associations]
+    model/stl=orca-slicer-dri.desktop;
+    model/3mf=orca-slicer-dri.desktop;
+    application/vnd.ms-3mfdocument=orca-slicer-dri.desktop;
+    application/prs.wavefront-obj=orca-slicer-dri.desktop;
+    application/x-amf=orca-slicer-dri.desktop;
+  '';
+
+  orcaSlicerMimeappsList = pkgs.writeText "orca-slicer-mimeapps.list" mimeappsListContent;
+in {
   imports = [
     # Hardware imports
     ./hardware-configuration.nix
@@ -23,6 +63,15 @@
   networking.hostName = "desktop";
   services.fwupd.enable = true;
   services.libinput.enable = true;
+
+  boot.plymouth = {
+    theme = "lone";
+    themePackages = with pkgs; [
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [ "lone" ];
+      })
+    ];
+  };
 
   # Copy current monitors.xml into GDM
   systemd.tmpfiles.rules = [
@@ -52,6 +101,12 @@
   };
   services.hardware.openrgb.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
   environment.systemPackages = with pkgs; [
     keepassxc
     wineWowPackages.waylandFull
@@ -64,6 +119,10 @@
     lsfg-vk
     vulkan-tools
 
+    distrobox
+
+    orcaSlicerDesktopItem
+
     (pkgs.heroic.override {
       extraPkgs = pkgs:
         with pkgs; [
@@ -73,7 +132,21 @@
         ];
     })
   ];
+
+  environment.sessionVariables = {
+    FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+  };
+
   services.flatpak.enable = true;
+
+  environment.etc."xdg/mimeapps.list".source = orcaSlicerMimeappsList;
+  environment.etc."xdg/mimeapps.list".mode = "0644";
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
 
   services.xserver.xkb.extraLayouts."EurKEY-Colemak" = {
     symbolsFile = ./EurKeyXKB;
