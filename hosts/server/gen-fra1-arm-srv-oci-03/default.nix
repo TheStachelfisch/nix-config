635278538502ff291187f09402e4180aaf2933d3
TheStachelfisch <contact@thestachelfisch.dev>
2025-09-11 02:58:23 +0200

    add new server

---
+++ hosts/server/gen-fra1-arm-srv-oci-03/default.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../common/global
+    ../common/users/thestachelfisch
+    ../common/optional/server
+  ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # TODO: Move to server module. Needed for remote deployment
+  security.sudo.wheelNeedsPassword = false;
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "gen-fra1-arm-srv-oci-03";
+
+  system.stateVersion = "23.11";
+}
