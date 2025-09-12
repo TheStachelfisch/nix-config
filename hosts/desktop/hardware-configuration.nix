555bfde510e0d4d72551f9564c2d01fba4bf87a4
TheStachelfisch <contact@thestachelfisch.dev>
2025-09-13 00:22:55 +0200

    update nvidia driver

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -52,6 +52,14 @@
   hardware.nvidia = {
     open = false;
     powerManagement.enable = true;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.82.09";
+      sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+      sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+      openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+      settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+      persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+    };
   };
 
   fileSystems."/" = {
