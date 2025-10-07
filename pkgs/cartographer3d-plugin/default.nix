f62e409787b2ba6e615c4791f62f89339a7b20c9
TheStachelfisch <contact@thestachelfisch.dev>
2025-10-08 01:23:46 +0200

    update cartographer plugin package

--- pkgs/cartographer3d-plugin/default.nix
+++ pkgs/cartographer3d-plugin/default.nix
@@ -6,23 +6,24 @@
   hatch-vcs,
   git,
   numpy,
+  scipy,
   typing-extensions
 }:
 buildPythonPackage rec {
   pname = "cartographer3d-plugin";
-  version = "v0.4.5";
+  version = "v0.7.3";
 
   src = fetchFromGitHub {
     owner = "Cartographer3D";
     repo = "cartographer3d-plugin";
     rev = version;
-    hash = "sha256-MJqz7P5a2e4sO2jFxcu8nhfhMiqA4TjoIQCQG0Z66tM=";
+    hash = "sha256-Rn/hmbCGOGHF1eNMVH/UxejqpLm+EgW6w/7f/HnK7Mk=";
   };
 
   pyproject = true;
 
   build-system = [hatchling hatch-vcs git];
-  propagatedBuildInputs = [numpy typing-extensions];
+  propagatedBuildInputs = [numpy typing-extensions scipy];
 
   patchPhase = ''
     sed -i -e 's/numpy~=1.16/numpy~=2.3/g' pyproject.toml
