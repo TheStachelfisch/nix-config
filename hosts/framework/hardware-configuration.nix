7427791af6a4422ff03a1868b1776dc0cc964009
TheStachelfisch <contact@thestachelfisch.dev>
2025-10-09 09:29:04 +0200

    add swap and zram to framework

--- hosts/framework/hardware-configuration.nix
+++ hosts/framework/hardware-configuration.nix
@@ -64,7 +64,15 @@
     fileSystems = ["/"];
   };
 
-  swapDevices = [{device = "/swap/swapfile";}];
+  swapDevices = [{
+    device = "/swap/swapfile";
+    priority = 0;
+  }];
+
+  zramSwap = {
+    enable = true;
+    priority = 5;
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
