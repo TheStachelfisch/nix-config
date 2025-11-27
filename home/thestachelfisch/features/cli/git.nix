154abf701fda9ae66683008dd5e504ee376f6b01
TheStachelfisch <contact@thestachelfisch.dev>
2025-11-27 08:45:41 +0100

    switch to git full

--- home/thestachelfisch/features/cli/git.nix
+++ home/thestachelfisch/features/cli/git.nix
@@ -8,7 +8,7 @@
 
   programs.git = {
     enable = true;
-    package = pkgs.git.override { withLibsecret = true; };
+    package = pkgs.gitFull;
     signing.key = "BAD3CD149697F0C7B922824FACA965618087B276";
     signing.signByDefault = true;
     settings = {
