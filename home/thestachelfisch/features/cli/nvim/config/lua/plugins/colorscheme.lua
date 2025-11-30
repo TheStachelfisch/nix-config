faf6fedf527309e41a3eea501c7a57032e9dcdfd
TheStachelfisch <contact@thestachelfisch.dev>
2025-11-30 03:09:53 +0100

    update Neovim color scheme

--- home/thestachelfisch/features/cli/nvim/config/lua/plugins/colorscheme.lua
+++ home/thestachelfisch/features/cli/nvim/config/lua/plugins/colorscheme.lua
@@ -4,6 +4,41 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      local C = require("nightfox.lib.color")
+
+      -- default bg color for Carbonfox
+      local bg = C("#161616");
+
+      require("nightfox").setup({
+        options = {
+          dim_inactive = true,
+          styles = {
+            comments = "italic",
+            keywords = "italic",
+          },
+        },
+        palettes = {
+          carbonfox = {
+            bg0 = bg:brighten(-4):to_css(),
+            bg1 = bg:brighten(-6):to_css(),
+          },
+        },
+        groups = {
+          all = {
+            Conditional = { style = "italic" },
+            Repeat = { style = "italic" },
+            PreProc = { style = "italic" },
+            ["@parameter"] = { style = "underline" },
+            ["@keyword.function"] = { style = "italic" },
+            ["@keyword.operator"] = { style = "italic" },
+            ["@keyword.operator.new"] = { style = "italic" },
+            ["@keyword.operator.borrow.and.rust"] = { style = "italic" },
+            ["@function.builtin"] = { style = "bold" },
+            ["@variable.builtin"] = { style = "bold" },
+          },
+        },
+      });
+
       vim.cmd([[colorscheme carbonfox]])
       vim.o.winborder = 'rounded'
     end,
