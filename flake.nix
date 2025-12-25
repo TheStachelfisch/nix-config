e89dc6d90da2918c9de17d9b216705f2bbade3c0
TheStachelfisch <contact@thestachelfisch.dev>
2025-12-25 02:13:46 +0100

    update nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   inputs = {
     # Nix Ecosystem
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     systems.url = "github:nix-systems/default-linux";
     nur = {
