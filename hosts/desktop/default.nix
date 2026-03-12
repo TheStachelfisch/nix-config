486a80c514dfe8eca523200bb54cd6134c9e3fd8
TheStachelfisch <contact@thestachelfisch.dev>
2026-03-12 12:17:08 +0100

    remove orca slicer zink override

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,44 +1,5 @@
-{pkgs, lib, config, ...}: let
-  orcaSlicerDesktopItem = pkgs.makeDesktopItem {
-    name = "orca-slicer-zink";
-    desktopName = "OrcaSlicer";
-    genericName = "3D Printing Software";
-    icon = "OrcaSlicer";
-    exec = "env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1 ${pkgs.orca-slicer}/bin/orca-slicer %U";
-    terminal = false;
-    type = "Application";
-    mimeTypes = [
-      "model/stl"
-      "model/3mf"
-      "application/vnd.ms-3mfdocument"
-      "application/prs.wavefront-obj"
-      "application/x-amf"
-      "x-scheme-handler/orcaslicer"
-    ];
-    categories = [ "Graphics" "3DGraphics" "Engineering" ];
-    keywords = [ "3D" "Printing" "Slicer" "slice" "3D" "printer" "convert" "gcode" "stl" "obj" "amf" "SLA" ];
-    startupNotify = false;
-    startupWMClass = "orca-slicer";
-  };
-
-  mimeappsListContent = ''
-    [Default Applications]
-    model/stl=orca-slicer-dri.desktop;
-    model/3mf=orca-slicer-dri.desktop;
-    application/vnd.ms-3mfdocument=orca-slicer-dri.desktop;
-    application/prs.wavefront-obj=orca-slicer-dri.desktop;
-    application/x-amf=orca-slicer-dri.desktop;
-
-    [Added Associations]
-    model/stl=orca-slicer-dri.desktop;
-    model/3mf=orca-slicer-dri.desktop;
-    application/vnd.ms-3mfdocument=orca-slicer-dri.desktop;
-    application/prs.wavefront-obj=orca-slicer-dri.desktop;
-    application/x-amf=orca-slicer-dri.desktop;
-  '';
-
-  orcaSlicerMimeappsList = pkgs.writeText "orca-slicer-mimeapps.list" mimeappsListContent;
-in {
+{pkgs, lib, config, ...}:
+{
   imports = [
     # Hardware imports
     ./hardware-configuration.nix
@@ -136,7 +97,7 @@ in {
 
     distrobox
 
-    orcaSlicerDesktopItem
+    orca-slicer
 
     (pkgs.heroic.override {
       extraPkgs = pkgs:
@@ -154,13 +115,9 @@ in {
 
   services.flatpak.enable = true;
 
-  environment.etc."xdg/mimeapps.list".source = orcaSlicerMimeappsList;
-  environment.etc."xdg/mimeapps.list".mode = "0644";
-
   security.lsm = lib.mkForce [ ];
   virtualisation.podman = {
     enable = true;
-    dockerCompat = true;
     defaultNetwork.settings.dns_enabled = true;
   };
 
