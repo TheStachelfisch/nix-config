16ca4381bfe2a33f9ed4e60accbd8484b7cc7fd5
TheStachelfisch <contact@thestachelfisch.dev>
2026-03-16 04:36:05 +0100

    add udev rule for no deadzone for 8bitdo Ultimate 2 controller in dinput mode

--- hosts/common/global/hardware.nix
+++ hosts/common/global/hardware.nix
@@ -1,3 +1,9 @@
-{...}: {
+{ pkgs, ... }: {
   hardware.enableRedistributableFirmware = true;
+
+  services.udev.extraRules = ''
+    # Replace the idVendor and idProduct with your specific controller IDs
+    # This rule triggers on 'add' and 'change' events to handle reconnects
+    ACTION=="add|change", KERNEL=="event*", SUBSYSTEM=="input", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="6012", RUN+="${pkgs.linuxConsoleTools}/bin/evdev-joystick --evdev /dev/input/%k --deadzone 0"
+  '';
 }
